6e4222b9ac49c17060e71f48d6a3290d7efa7237
Fernando Soto <zomaotoko@gmail.com>
2018-06-05 10:53:14 -0500

    Add reclycler view and pokeball icon

--- app/src/main/java/com/zomaotoko/pokedex/MainActivity.kt
+++ app/src/main/java/com/zomaotoko/pokedex/MainActivity.kt
@@ -2,13 +2,20 @@ package com.zomaotoko.pokedex
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = getColor(R.color.colorPrimaryDark)
+        setSupportActionBar(toolbar)
+        updateToolbar()
+    }
 
-
+    private fun updateToolbar() {
+        toolbar.navigationIcon = getDrawable(R.drawable.ic_pokeball)
+        invalidateOptionsMenu()
     }
 }
